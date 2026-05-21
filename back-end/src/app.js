dc8abb8a82df58c63b90b4c89cc3a2feff570058
AY-Dannex <ayomidedaniel1212@gmail.com>
2026-05-21 18:44:55 +0100

    Removed cors logic

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,4 +1,3 @@
-import cors from "cors"
 import express from "express"
 import userRouter from "./routes/users.route.js"
 import postRouter from "./routes/post.route.js"
@@ -10,18 +9,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    origin: function(origin, callback) {
-        callback(null, true)  // allow all origins temporarily
-    },
-    credentials: true
-}))
-
 app.use("/api/user/", userRouter)
 app.use("/api/post/", postRouter)
 app.use("/api/activity-logs/", activityRouter)
 
 export default app
-
-//route: https://localhost:5000/api/user/register
