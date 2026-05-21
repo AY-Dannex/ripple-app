e3b74f03f913ae8b284a6142e56b18d3a052844e
AY-Dannex <ayomidedaniel1212@gmail.com>
2026-05-21 20:19:31 +0100

    fixed stuff

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -26,7 +26,7 @@ app.use("/api/activity-logs/", activityRouter)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "dist")))
-    app.get("*", (req, res) => {
+    app.get("/{*splat}", (req, res) => {
         res.sendFile(path.join(__dirname, "dist", "index.html"))
     })
 }
